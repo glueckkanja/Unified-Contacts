8e68863985f5491290e095a65a5fe07122d92032
Stefan Heller <88711548+StefanH114@users.noreply.github.com>
2026-09-24 15:12:24 +0200

    feat: skip delete users in favorites (#35)

--- UnifiedContacts/UnifiedContacts.Server/Engines/SearchEngines/AzureADSearchEngine.cs
+++ UnifiedContacts/UnifiedContacts.Server/Engines/SearchEngines/AzureADSearchEngine.cs
@@ -136,6 +136,11 @@ namespace UnifiedContacts.Engines.SearchEngines
                         {
                             throw new UnauthorizedAccessException("Forbidden");
                         }
+                        // Favorited user was deleted; skip it instead of failing the whole favorites request.
+                        else if (e.ResponseStatusCode == StatusCodes.Status404NotFound)
+                        {
+                            continue;
+                        }
                         else
                         {
                             throw;
